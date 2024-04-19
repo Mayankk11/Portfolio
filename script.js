0942ffec6f789aa72101dfbbb07797edd54b6d6b
Mayankk11 <72143980+Mayankk11@users.noreply.github.com>
2024-04-20 01:38:20 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    // script for home text animation : 
+var typed = new Typed('#element', {
+    strings: ['I am a full stack web developer.', 'I am a passinated android app developer.'],
+    typeSpeed: 60,
+});
